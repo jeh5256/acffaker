11e07254efc5617524d61f958d1d66e17d35978e
Justin <jhughes@cliquestudios.com>
2020-01-16 14:58:33 -0600

    Fix require and begin to handle filling in fields with Faker data

--- HandleFields/HandleFields.php
+++ HandleFields/HandleFields.php
@@ -2,12 +2,14 @@
 
 namespace HandleFields;
 
-//require_once '../AcfFill/AcfFill.php';
+require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/acf-faker/QueryPosts/QueryPosts.php');
+require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/acf-faker/AcfFill/AcfFill.php');
 
 use AcfFill\AcfFill;
 
 class HandleFields
 {
+
     public static function handle($ids=[], $fields=[])
     {
         if (!\is_array($ids) || !\is_array($fields)) {
@@ -16,11 +18,31 @@ class HandleFields
 
         $faker = new AcfFill();
 
-        foreach ($fields as $field) {
-            switch ($field) {
-                case 'text';
-                    $text = $faker->fillText($field['maxlength'], $field['default_value']);
-                    update_field(' ')
+        foreach ($ids as $id) {
+            foreach ($fields as $field) {
+                switch ($field['type']) {
+                    case 'text':
+                        $content = $faker->fillText($field['maxlength'], $field['default_value']);
+
+                        break;
+
+                    case 'number':
+                        $content = $faker->fillNumber($field['default_value'], $field['min'], $field['max']);
+
+                        break;
+
+                    default:
+                        $content = '';
+                }
+
+                if (!empty(\get_post($id))) {
+                    \var_dump($field['name']);
+                    \var_dump($field['type'] === 'text');
+                    \var_dump('content' . $content);
+                    \var_dump('id' . \intval($id));
+                    $a = \update_field($field['name'], $content, \intval($id));
+                    \var_dump('status' . $a);
+                }
             }
         }
     }
