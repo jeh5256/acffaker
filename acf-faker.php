a7d20f5701d9fe570befa67035bf09e010a445cb
Justin <jhughes@cliquestudios.com>
2020-01-16 14:59:13 -0600

    Debuging

--- acf-faker.php
+++ acf-faker.php
@@ -1,9 +1,15 @@
 <?php
+
 require_once '../wp-load.php';
 require_once './QueryPosts/QueryPosts.php';
+require_once './HandleFields/HandleFields.php';
+
 use QueryPosts\QueryPosts;
+use AcfFill\AcfFill;
+use HandleFields\HandleFields;
+
 $acf_json_path = get_template_directory() . '/acf-json';
-//var_dump($acf_json_path);
+var_dump(realpath(dirname(__FILE__)));
 
 if ($handle = opendir($acf_json_path)) {
 
@@ -14,20 +20,27 @@ if ($handle = opendir($acf_json_path)) {
                $json = json_decode(file_get_contents($acf_json_path . '/' . $entry), true);
                $query_posts = new QueryPosts();
 //                echo '<pre>';
-//                var_dump($json);
+//                var_dump($json['fields']);
 //                echo '</pre>';
                if (array_key_exists('location', $json)) {
                    foreach ($json['location'] as $location) {
-                        echo $location[0]['param'] . '<br/>';
+                       //var_dump($location[0]['param']);
+                        //echo'========= <br/>';
+//                        echo '<pre>';
+//                        var_dump($location[0]);
+//                        echo '</pre>';
                        switch ($location[0]['param']) {
                            case 'page_template':
                                $page_template = $location[0]['value'];
 
                                $page_ids = $query_posts->getPostIdsByTemplate($page_template);
 
-                               echo '<pre>';
-                               var_dump($page_ids);
-                               echo '</pre>';
+//                               if (!empty($page_ids) && array_key_exists('fields', $json)) {
+//                                   HandleFields::
+//                               }
+//                               echo '<pre>';
+//                               var_dump($page_ids);
+//                               echo '</pre>';
 
                                break;
                            case 'post_type':
@@ -36,9 +49,14 @@ if ($handle = opendir($acf_json_path)) {
 
 
                                $post_ids = $query_posts->getIdsByPostType($post_template);
-                               echo '<pre>';
-                               var_dump($post_ids);
-                               echo '</pre>';
+
+                               if (!empty($post_ids)) {
+                                   HandleFields::handle($post_ids, $json['fields']);
+                               }
+//                               echo '<pre>';
+//                               var_dump($post_ids);
+//                               echo '<br/>';
+//                               echo '</pre>';
                                break;
                            default:
                                //print_r($location[0]);
@@ -52,8 +70,6 @@ if ($handle = opendir($acf_json_path)) {
 
     closedir($handle);
 }
-require_once 'AcfFill.php';
-use AcfFill\AcfFill;
 
 $a = new AcfFill();
 //var_dump($a->fillText(20, '', 1111, 222));
@@ -61,4 +77,4 @@ $a = new AcfFill();
 //var_dump($a->fillEmail());
 //var_dump($a->fillUrl());
 //var_dump($a->fillPassword());
-var_dump($a->fillImage());
+//var_dump($a->fillImage());
