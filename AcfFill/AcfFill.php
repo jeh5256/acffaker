5b86609d6c6443ed96141c9b958d026ea82e5377
Justin <jhughes@cliquestudios.com>
2020-01-30 15:08:01 -0600

    Add functions to upload or find exiting images

--- AcfFill/AcfFill.php
+++ AcfFill/AcfFill.php
@@ -4,17 +4,29 @@ namespace AcfFill;
 
 //require_once __DIR__ . '../vendor/autoload.php';
 require_once(realpath(dirname(__FILE__)) . '../../vendor/autoload.php');
+require_once( ABSPATH . 'wp-admin/includes/image.php' );
+require_once( \ABSPATH . 'wp-includes/class-wp-query.php');
 
 use Faker\Factory as Faker;
 
 class AcfFill
 {
     protected $faker;
+    protected $wp_query;
 
-    public function fillText($maxlength = 10, $default_value = '')
+    public function __construct()
+    {
+        $this->faker = Faker::create();
+    }
+
+    public function fillText($maxlength, $default_value = '')
     {
         if (!empty($default_value)) {
             return $default_value;
+        }
+
+        if (empty($maxlength)) {
+            $maxlength = 10;
         }
 
         return $this->faker->sentence($maxlength);
@@ -26,10 +38,10 @@ class AcfFill
             return $default_value;
         }
 
-        return $number = $this->faker->numberBetween($min, $max);
+        return $number = $this->faker->numberBetween(\intval($min), intval($max));
     }
 
-    public function fillEmail($default_value = '')
+    public function fillEmail($default_value = 'jdoe@aol.com')
     {
         if (!empty($default_value)) {
             return $default_value;
@@ -48,9 +60,66 @@ class AcfFill
         return $this->faker->password();
     }
 
+    public function uploadImageFromUrl($url)
+    {
+        $upload_dir = wp_upload_dir();
+        $image_data = file_get_contents($url);
+        $filename = basename($url);
+
+        if (wp_mkdir_p( $upload_dir['path'])) {
+            $file = $upload_dir['path'] . '/' . $filename;
+        }
+        else {
+            $file = $upload_dir['basedir'] . '/' . $filename;
+        }
+
+        file_put_contents($file, $image_data);
+
+        $wp_filetype = wp_check_filetype($filename, null);
+
+        $attachment = array(
+            'post_mime_type' => $wp_filetype['type'],
+            'post_title' => sanitize_file_name( $filename ),
+            'post_content' => '',
+            'post_status' => 'inherit'
+        );
+
+        $attach_id = wp_insert_attachment( $attachment, $file );
+        $attach_data = wp_generate_attachment_metadata($attach_id, $file);
+        wp_update_attachment_metadata($attach_id, $attach_data);
+
+        return $attach_id;
+    }
+
+    public function checkForExistingImages()
+    {
+        $args = [
+            'post_type'      => 'attachment',
+            'post_mime_type' => 'image',
+            'post_status'    => 'inherit',
+            'posts_per_page' => 1,
+        ];
+
+        $query_images = new \WP_Query($args);
+
+        if (count($query_images->posts) > 0) {
+            return $query_images[0]->ID;
+        } else {
+            return null;
+        }
+
+    }
+
     public function fillImage($width = 500, $height = 500)
     {
-        return $this->faker->imageUrl($width, $height);
+        $existing_images = $this->checkForExistingImages();
+        if (count($existing_images) > 0) {
+            return $existing_images;
+        }
+        
+        $image_url = $this->faker->imageUrl($width, $height);
+
+        return $this->uploadImageFromUrl($image_url);
     }
 
     public function fillFile($type = 'pdf')
