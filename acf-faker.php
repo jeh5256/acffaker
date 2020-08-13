2577d15ffc59553a8e7f1f178b4c98fd3d5aee9c
Justin <jhughes@cliquestudios.com>
2020-08-13 15:53:26 -0500

    Remove dev debugging and move to class

--- acf-faker.php
+++ acf-faker.php
@@ -5,78 +5,108 @@ require_once './QueryPosts/QueryPosts.php';
 require_once './HandleFields/HandleFields.php';
 
 use QueryPosts\QueryPosts;
-use AcfFill\AcfFill;
 use HandleFields\HandleFields;
 
 ini_set('max_execution_time', 120);
 
-$acf_json_path = get_template_directory() . '/acf-json';
-var_dump(realpath(dirname(__FILE__)));
+class ACFFaker {
 
-if ($handle = opendir($acf_json_path)) {
+    protected $acf_json_path;
 
-    while (false !== ($entry = readdir($handle))) {
-        if ($entry != "." && $entry != "..") {
+    public function __construct()
+    {
+        $this->acf_json_path = get_template_directory() . '/acf-json';
+    }
 
-           try {
-               $json = json_decode(file_get_contents($acf_json_path . '/' . $entry), true);
-               $query_posts = new QueryPosts();
-//                echo '<pre>';
-//                var_dump($json['fields']);
-//                echo '</pre>';
-               if (array_key_exists('location', $json)) {
-                   foreach ($json['location'] as $location) {
-                       //var_dump($location[0]['param']);
-                        //echo'========= <br/>';
-//                        echo '<pre>';
-//                        var_dump($location[0]);
-//                        echo '</pre>';
-                       switch ($location[0]['param']) {
-                           case 'page_template':
-                               $page_template = $location[0]['value'];
+    public function fillAll() {
+        if ($handle = opendir($this->acf_json_path)) {
 
-                               $page_ids = $query_posts->getPostIdsByTemplate($page_template);
+            while (false !== ($entry = readdir($handle))) {
+                if ($entry !== "." && $entry !== "..") {
 
-//                               if (!empty($page_ids) && array_key_exists('fields', $json)) {
-//                                   HandleFields::
-//                               }
-//                               echo '<pre>';
-//                               var_dump($page_ids);
-//                               echo '</pre>';
+                    try {
+                        $json = json_decode(file_get_contents($this->acf_json_path . '/' . $entry), true);
+                        $query_posts = new QueryPosts();
 
-                               break;
-                           case 'post_type':
-                               $post_template = $location[0]['value'];
-                               //echo $post_template . '<br>';
+                        if (array_key_exists('location', $json)) {
+                            foreach ($json['location'] as $location) {
 
+                                switch ($location[0]['param']) {
+                                    case 'page_template':
+                                        $page_template = $location[0]['value'];
+                                        $page_ids = QueryPosts::getPostIdsByTemplate($page_template);
 
-                               $post_ids = $query_posts->getIdsByPostType($post_template);
+                                        break;
+                                    case 'post_type':
+                                        $post_template = $location[0]['value'];
+                                        $post_ids = QueryPosts::getIdsByPostType($post_template);
 
-                               if (!empty($post_ids)) {
-                                   HandleFields::handle($post_ids, $json['fields']);
-                               }
-//                               echo '<pre>';
-//                               var_dump($post_ids);
-//                               echo '<br/>';
-//                               echo '</pre>';
-                               break;
-                           default:
-                               //print_r($location[0]);
-                       }
-                       //var_dump($location[0]);
-                   }
-               }
-           } catch(Exception $e) { echo $e; }
+                                        if (!empty($post_ids)) {
+                                            HandleFields::handle($post_ids, $json['fields']);
+                                        }
+                                        break;
+                                    default:
+                                }
+                            }
+                        }
+                    } catch(Exception $e) { echo $e; }
+                }
+            }
+
+            closedir($handle);
         }
     }
 
-    closedir($handle);
-}
+    public function fillByIdOrType($ids=[], $types=[])
+    {
+        try {
+            if (!is_array($ids) || !is_array($types)) return;
 
-$a = new AcfFill();
-//var_dump($a->fillText(20, '', 1111, 222));
-//var_dump($a->fillNumber(null, 20, 21, '<li>', '</li>'));
-//var_dump($a->fillEmail());
-//var_dump($a->fillUrl());
-//var_dump($a->fillPassword());
-//var_dump($a->fillImage());
+            /**
+             * Can skip any querying here since types is empty and acf only needs ids
+             **/
+
+            if (!empty($types) && !empty($ids)) {
+                $queried_ids = QueryPosts::getPostsByTypeAndId($ids, $types);
+
+                if (!empty($queried_ids)) {
+                    foreach ($queried_ids as $queried_id) {
+                        $fields = get_field_objects($queried_id);
+
+                        if (!empty($fields)) {
+                            HandleFields::handle($queried_ids, $fields);
+                        }
+                    }
+
+                }
+            }
+
+            if (empty($types) && !empty($ids)) {
+                foreach ($ids as $id) {
+                    $fields = get_field_objects($id);
+
+                    if ($fields) {
+                        HandleFields::handle($ids, $fields);
+                    }
+                }
+            }
+
+
+
+            if (!empty($types) && empty($ids)) {
+                $queried_ids = QueryPosts::getPostsByType($types);
+
+                if (!empty($queried_ids)) {
+                    foreach ($queried_ids as $queried_id) {
+                        $fields = get_field_objects($queried_id);
+
+                        if (!empty($fields)) {
+                            HandleFields::handle($queried_ids, $fields);
+                        }
+                    }
+                }
+            }
+
+        } catch(Exception $e) { echo $e; }
+    }
+}
