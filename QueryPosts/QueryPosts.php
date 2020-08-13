42aae6b9638637242ffab531c77eac6569950fb4
Justin <jhughes@cliquestudios.com>
2020-08-13 15:53:02 -0500

    Refactor to make all functions static and add functions to get posts by id and type

--- QueryPosts/QueryPosts.php
+++ QueryPosts/QueryPosts.php
@@ -7,39 +7,69 @@ namespace QueryPosts;
 
 class QueryPosts
 {
-    private $wpdb;
-    private $table_prefix;
-
-    public function __construct()
+    public static function getPostIdsByTemplate($page_template)
     {
         global $wpdb;
-        $this->wpdb = $wpdb;
-        $this->table_prefix = $this->wpdb->prefix;
-    }
 
-    public function getPostIdsByTemplate($page_template)
-    {
-        $table_name = $this->wpdb->prefix . "postmeta";
-
-        $sql = $this->wpdb->prepare(
-            "SELECT post_id FROM {$table_name} WHERE meta_key LIKE '%_wp_page_template%' AND meta_value=%s",
+        $sql = $wpdb->prepare(
+            "SELECT post_id FROM {$wpdb->postmeta} WHERE meta_key LIKE '%_wp_page_template%' AND meta_value=%s",
             $page_template
         );
 
-        $results = $this->wpdb->get_results($sql , ARRAY_A);
+        $results = $wpdb->get_results($sql , ARRAY_A);
 
-        return \wp_list_pluck($results, 'post_id');
+        return wp_list_pluck($results, 'post_id');
     }
 
-    public function getIdsByPostType($post_type)
+    public static function getIdsByPostType($post_type)
     {
-        $table_name = $this->wpdb->prefix . "posts";
+        global $wpdb;
 
-        $sql = $this->wpdb->prepare(
-            "SELECT id FROM {$table_name} WHERE post_type=%s",
+        $sql = $wpdb->prepare(
+            "SELECT id FROM {$wpdb->posts} WHERE post_type=%s",
             $post_type
         );
 
-        return \wp_list_pluck($this->wpdb->get_results($sql , ARRAY_A), 'id');
+        return \wp_list_pluck($wpdb->get_results($sql , ARRAY_A), 'id');
+    }
+
+    public static function getPostsByType($types=[])
+    {
+        global $wpdb;
+
+        if (!is_array($types)) return [];
+
+        $types = self::convert_to_sql_ready_string($types);
+
+        $sql = $wpdb->prepare(
+            "SELECT id FROM {$wpdb->posts} WHERE post_type IN ( $types )", [
+                $types
+            ]
+        );
+
+        return \wp_list_pluck($wpdb->get_results($sql , ARRAY_A), 'id');
+    }
+
+    public static function getPostsByTypeAndId($ids=[], $types=[])
+    {
+        global $wpdb;
+
+        if (!is_array($ids) && !is_array($types)) return [];
+
+        $types = self::convert_to_sql_ready_string($types);
+        $ids = self::convert_to_sql_ready_string($ids);
+
+
+        $sql = $wpdb->prepare(
+            "SELECT id FROM {$wpdb->posts} WHERE post_type IN ( $types ) AND ID in ( $ids )", [
+                $types, $ids
+            ]
+        );
+
+        return \wp_list_pluck($wpdb->get_results($sql , ARRAY_A), 'id');
+    }
+
+    private static function convert_to_sql_ready_string( $arr ) {
+        return '"' . implode( $arr, '","' ) . '"';
     }
 }
